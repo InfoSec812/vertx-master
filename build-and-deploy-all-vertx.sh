e7d56d9ac05560b1c53957037ab5d67feaa85d99
Deven Phillips <deven.phillips@gmail.com>
2015-03-16 11:28:56 -0400

    Updated some of the scripts

--- build-and-deploy-all-vertx.sh
+++ build-and-deploy-all-vertx.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+BASEDIR=${PWD}
 for I in `cat vertx-repo-list.txt | awk -F":" '{print $2}'`
 do 
-	cd ~/Documents/Zanclus/Workspace/${I}
+	cd ${BASEDIR}/${I}
 	git checkout master
 	git fetch --all
 	git merge upstream/master
 	git push origin master
 	mvn clean package -DskipTests
 	mvn deploy -DskipTests -DaltDeploymentRepository=nexus-snapshots::default::https://home.zanclus.com/nexus/content/repositories/snapshots/
-	cd ~/Documents/Zanclus/Workspace
+	cd ${BASEDIR}
 done
--- update-all-vertx.sh
+++ update-all-vertx.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-source ~/.bashrc
+BASEDIR=${PWD}
 for I in `cat vertx-repo-list.txt | awk -F":" '{print $2}'`
 do
     echo "Updating ${I}"
-    cd /home/dphillips/Documents/Zanclus/Workspace/${I}
+    cd ${BASEDIR}/${I}
     git checkout master
     git fetch --all
     git reset --hard
     mvn clean
     git merge upstream/master
     git push origin master
-    cd /home/dphillips/Documents/Zanclus/Workspace
+    cd ${BASEDIR}
 done
